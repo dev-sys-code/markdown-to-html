0743a04e3f61501ac6481a3626685733af3374d2
dev-sys-code <kaydenbuster@outlook.com>
2026-07-20 21:28:31 +0100

    logic fixed

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,17 @@ int main() {
     using std::ofstream;
     
     vector<string> keywords{};
-    string user_markdown{};
+    string userMarkdown{};
     string result{};
+    string fileName{};
 
-    ofstream file("html.txt");
-    getline(cin, user_markdown);
+    cout << "Enter markdown file name:\n  >_";
+    getline(cin, fileName);
+    ofstream file(fileName);
 
-    if (user_markdown.rfind("###", 0) == 0) { result.append("<h3>" + user_markdown.substr(3) + "</h3>"); }
+    if (userMarkdown.rfind("###", 0) == 0) { result.append("<h3>" + userMarkdown.substr(4) + "</h3>"); }
+    if (userMarkdown.rfind("##", 0) == 0) { result.append("<h2>" + userMarkdown.substr(3) + "</h2>"); }
+    if (userMarkdown.rfind("#", 0) == 0) { result.append("<h1>" + userMarkdown.substr(2) + "</h1>"); }
 
     cout << result;
 }
