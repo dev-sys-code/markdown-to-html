8bb816b05cbcc916ec530997c98b15b97ac6fd1c
dev-sys-code <kaydenbuster@outlook.com>
2026-07-20 21:46:51 +0100

    logic + scope to add file

--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,23 @@ int main() {
 
     cout << "Enter markdown file name:\n  >_";
     getline(cin, fileName);
-    ifstream mdFile(fileName);
+
+    {
+        ofstream mdFile(fileName + ".md");
+    }
+
+    ifstream mdFile(fileName + ".md");
     ofstream htmlFile("output.html");
 
     if (!mdFile.is_open()) { cout << "Unknown error opening markdown file.\n"; }
-    if (!htmlFile.is_open()) { cout << "Unknown error opening markdown file.\n"; }
+    if (!htmlFile.is_open()) { cout << "Unknown error opening html file.\n"; }
 
+    string line{};
 
-    if (userMarkdown.rfind("###", 0) == 0) { result.append("<h3>" + userMarkdown.substr(4) + "</h3>"); }
-    if (userMarkdown.rfind("##", 0) == 0) { result.append("<h2>" + userMarkdown.substr(3) + "</h2>"); }
-    if (userMarkdown.rfind("#", 0) == 0) { result.append("<h1>" + userMarkdown.substr(2) + "</h1>"); }
+    while (getline(mdFile, line)) {
+        string result{""};
+        if (line.rfind("###", 0) == 0) { result.append("<h3>" + line.substr(4) + "</h3>"); }
+        else if (line.rfind("##", 0) == 0) { result.append("<h2>" + line.substr(3) + "</h2>"); }
+        else if (line.rfind("#", 0) == 0) { result.append("<h1>" + line.substr(2) + "</h1>"); }
+    }
 }
