99342b0738f9741a539bbd603478fd4d41af01ba
dev-sys-code <kaydenbuster@outlook.com>
2026-07-22 20:07:25 +0100

    fix logic

--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,34 @@ int main() {
     using std::ofstream;
     using std::ifstream;
     
-    vector<string> keywords{};
-    string userMarkdown{};
-    string result{};
     string fileName{};
 
-    cout << "Enter markdown file name:\n  >_";
+    cout << "Enter markdown file name (without extension):\n  >_";
     getline(cin, fileName);
-
-    {
-        ofstream mdFile(fileName + ".md");
-    }
 
     ifstream mdFile(fileName + ".md");
     ofstream htmlFile("output.html");
 
-    if (!mdFile.is_open()) { cout << "Unknown error opening markdown file.\n"; }
-    if (!htmlFile.is_open()) { cout << "Unknown error opening html file.\n"; }
+    if (!mdFile.is_open()) { 
+        cout << "Error opening markdown file. Make sure it exists!\n"; 
+        return 1;
+    }
+    if (!htmlFile.is_open()) { 
+        cout << "Error opening html file.\n"; 
+        return 1;
+    }
 
     string line{};
 
     while (getline(mdFile, line)) {
-        string result{""};
-        if (line.rfind("###", 0) == 0) { result.append("<h3>" + line.substr(4) + "</h3>"); }
-        else if (line.rfind("##", 0) == 0) { result.append("<h2>" + line.substr(3) + "</h2>"); }
-        else if (line.rfind("#", 0) == 0) { result.append("<h1>" + line.substr(2) + "</h1>"); }
+        if (line.rfind("###", 0) == 0) { 
+            htmlFile << "<h3>" + line.substr(4) + "</h3>\n"; 
+        }
+        else if (line.rfind("##", 0) == 0) { 
+            htmlFile << "<h2>" + line.substr(3) + "</h2>\n"; 
+        }
+        else if (line.rfind("#", 0) == 0) { 
+            htmlFile << "<h1>" + line.substr(2) + "</h1>\n"; 
+        }
     }
-
-    htmlFile << result;
 }
